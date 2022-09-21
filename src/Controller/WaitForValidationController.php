7c5e6d809513d42479d86f8e910435e2f395817b
Julien Labatut <skoognuts@outlook.fr>
2022-09-21 16:40:47 +0200

    Add logic controllers

---
+++ src/Controller/WaitForValidationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WaitForValidationController extends AbstractController
+{
+    #[Route('/wait/for/validation', name: 'app_wait_for_validation')]
+    public function index(): Response
+    {
+        return $this->render('wait_for_validation/index.html.twig', [
+            'controller_name' => 'WaitForValidationController',
+        ]);
+    }
+}
