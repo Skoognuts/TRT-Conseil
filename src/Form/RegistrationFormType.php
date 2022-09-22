e6a634c3b84b308bd8db2a31485fcc9d058916c2
Julien Labatut <skoognuts@outlook.fr>
2022-09-22 11:24:49 +0200

    Add user type field in registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -18,6 +19,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('userType', ChoiceType::class, [
+                'label' => false,
+                'mapped' => false,
+                'choices' => [
+                    'Candidat' => 'candidate',
+                    'Recruteur' => 'recruiter'
+                ]
+            ])
             ->add('email', EmailType::class, [
                 'label' => false
             ])
